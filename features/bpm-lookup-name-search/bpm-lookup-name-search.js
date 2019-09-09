acce23da50c2ff75863608b5745f9bd5894a3678
Yurii Babenko <babenkomahalych@gmail.com>
2019-09-09 11:55:51 +0300

    feature/lookup-name-search
    разбил код на функции

--- features/bpm-lookup-name-search/bpm-lookup-name-search.js
+++ features/bpm-lookup-name-search/bpm-lookup-name-search.js
@@ -3,6 +3,31 @@
 		global.simkData = global.simkData || {};
 	}
 
+	function getRequestBody() {
+		return `{"rootSchemaName":"SysSchema","operationType":0,"filters":{"items":{"ExtendParentFilter":{"filterType":1,"comparisonType":3,"isEnabled":true,"trimDateTimeParameterToDate":false,"leftExpression":{"expressionType":0,"columnPath":"ExtendParent"},"rightExpression":{"expressionType":2,"parameter":{"dataValueType":1,"value":false}}}},"logicalOperation":0,"isEnabled":true,"filterType":6},"columns":{"items":{"Id":{"caption":"","orderDirection":0,"orderPosition":-1,"isVisible":true,"expression":{"expressionType":0,"columnPath":"Id"}},"UId":{"caption":"","orderDirection":0,"orderPosition":-1,"isVisible":true,"expression":{"expressionType":0,"columnPath":"UId"}},"Name":{"caption":"","orderDirection":0,"orderPosition":-1,"isVisible":true,"expression":{"expressionType":0,"columnPath":"Name"}}}},"isDistinct":false,"rowCount":-1,"rowsOffset":-1,"isPageable":false,"allColumns":false,"useLocalization":true,"useRecordDeactivation":false,"serverESQCacheParameters":{"cacheLevel":0,"cacheGroup":"","cacheItemName":""},"queryOptimize":false,"useMetrics":false,"querySource":0,"ignoreDisplayValues":false,"isHierarchical":false}`;
+	}
+
+	function getRequestParams(body) {
+		function getCoockie(name) {
+			var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+			return match && match[2];
+		};
+
+		if (typeof body !== "string") {
+			body = JSON.stringify(body);
+		}
+		return {
+			"method": "POST",
+			"headers": {
+				"Content-Type": "application/json",
+				"BPMCSRF": getCoockie("BPMCSRF"),
+				"Timestamp": new Date().toISOString(),
+				"X-Request-Source": "ajax-provider",
+			},
+			body
+		};
+	}
+
 	function replace() {
 		const baseLoadData = Terrasoft.ComboBox.prototype.loadData;
 		Terrasoft.ComboBox.prototype.loadData = function(data) {
@@ -12,30 +37,15 @@
 			baseLoadData.apply(this, arguments);
 		};
 	}
-	
-	const getCoockie = name => {
-		var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-		return match && match[2];
-	};
-	
+
 	if (global && global.simkData && global.simkData.schemaNames) {
 		replace();
 		return;
 	}
 
 	init();
-	const body = `{"rootSchemaName":"SysSchema","operationType":0,"filters":{"items":{"ExtendParentFilter":{"filterType":1,"comparisonType":3,"isEnabled":true,"trimDateTimeParameterToDate":false,"leftExpression":{"expressionType":0,"columnPath":"ExtendParent"},"rightExpression":{"expressionType":2,"parameter":{"dataValueType":1,"value":false}}}},"logicalOperation":0,"isEnabled":true,"filterType":6},"columns":{"items":{"Id":{"caption":"","orderDirection":0,"orderPosition":-1,"isVisible":true,"expression":{"expressionType":0,"columnPath":"Id"}},"UId":{"caption":"","orderDirection":0,"orderPosition":-1,"isVisible":true,"expression":{"expressionType":0,"columnPath":"UId"}},"Name":{"caption":"","orderDirection":0,"orderPosition":-1,"isVisible":true,"expression":{"expressionType":0,"columnPath":"Name"}}}},"isDistinct":false,"rowCount":-1,"rowsOffset":-1,"isPageable":false,"allColumns":false,"useLocalization":true,"useRecordDeactivation":false,"serverESQCacheParameters":{"cacheLevel":0,"cacheGroup":"","cacheItemName":""},"queryOptimize":false,"useMetrics":false,"querySource":0,"ignoreDisplayValues":false,"isHierarchical":false}`;
-	const response = await fetch("../DataService/json/SyncReply/SelectQuery", {
-		"method": "POST",
-		"headers": {
-			"Content-Type": "application/json",
-			"BPMCSRF": getCoockie("BPMCSRF"),
-			"Timestamp": new Date().toISOString(),
-			"X-Request-Source": "ajax-provider",
-		},
-		body
-	});
-	const parsed = await response.json();
+	const body = getRequestBody();
+	const parsed = await fetch("../DataService/json/SyncReply/SelectQuery", getRequestParams(body)).then(r => r.json());
 	global.simkData.schemaNames = parsed.rows;
 	replace();
 })(window)
