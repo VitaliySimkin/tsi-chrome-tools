027e8a247693b5df6ded820bffa343f8befd1e2f
Yurii Babenko <babenkomahalych@gmail.com>
2019-09-09 11:53:43 +0300

    feature/lookup-name-search
    add init.
    add additional level to simkData variable

--- features/bpm-lookup-name-search/bpm-lookup-name-search.js
+++ features/bpm-lookup-name-search/bpm-lookup-name-search.js
@@ -1,9 +1,13 @@
 (async function(global) {
+	function init() {
+		global.simkData = global.simkData || {};
+	}
+
 	function replace() {
-		let baseLoadData = Terrasoft.ComboBox.prototype.loadData;
+		const baseLoadData = Terrasoft.ComboBox.prototype.loadData;
 		Terrasoft.ComboBox.prototype.loadData = function(data) {
 			if (this.columnName === "ReferenceSchemaUId") {
-				data.forEach(item => item[1] = global.simkData.find(r => r.UId === item[0]).Name || item[1]);
+				data.forEach(item => item[1] = global.simkData.schemaNames.find(r => r.UId === item[0]).Name || item[1]);
 			}
 			baseLoadData.apply(this, arguments);
 		};
@@ -13,7 +17,13 @@
 		var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
 		return match && match[2];
 	};
+	
+	if (global && global.simkData && global.simkData.schemaNames) {
+		replace();
+		return;
+	}
 
+	init();
 	const body = `{"rootSchemaName":"SysSchema","operationType":0,"filters":{"items":{"ExtendParentFilter":{"filterType":1,"comparisonType":3,"isEnabled":true,"trimDateTimeParameterToDate":false,"leftExpression":{"expressionType":0,"columnPath":"ExtendParent"},"rightExpression":{"expressionType":2,"parameter":{"dataValueType":1,"value":false}}}},"logicalOperation":0,"isEnabled":true,"filterType":6},"columns":{"items":{"Id":{"caption":"","orderDirection":0,"orderPosition":-1,"isVisible":true,"expression":{"expressionType":0,"columnPath":"Id"}},"UId":{"caption":"","orderDirection":0,"orderPosition":-1,"isVisible":true,"expression":{"expressionType":0,"columnPath":"UId"}},"Name":{"caption":"","orderDirection":0,"orderPosition":-1,"isVisible":true,"expression":{"expressionType":0,"columnPath":"Name"}}}},"isDistinct":false,"rowCount":-1,"rowsOffset":-1,"isPageable":false,"allColumns":false,"useLocalization":true,"useRecordDeactivation":false,"serverESQCacheParameters":{"cacheLevel":0,"cacheGroup":"","cacheItemName":""},"queryOptimize":false,"useMetrics":false,"querySource":0,"ignoreDisplayValues":false,"isHierarchical":false}`;
 	const response = await fetch("../DataService/json/SyncReply/SelectQuery", {
 		"method": "POST",
@@ -26,6 +36,6 @@
 		body
 	});
 	const parsed = await response.json();
-	global.simkData = parsed.rows;
+	global.simkData.schemaNames = parsed.rows;
 	replace();
 })(window)
