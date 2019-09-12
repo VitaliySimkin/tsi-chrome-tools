d70d6f5cc731af4f3ad8305dcc659e9d3d53a6b1
E.Andrienko <y.andriienko@upswot.com>
2019-09-12 14:53:00 +0300

    #5 Добавил try catch

--- features/bpm-open-page-script/index.js
+++ features/bpm-open-page-script/index.js
@@ -7,7 +7,9 @@ export default {
     "description": {
         "screen": "bpm-open-page-script.png",
         "text": {
-            "ru": "BPM. Открытие страницы в devtool",
+            "ru": `BPM. Открытие страницы в devtool. \r\n В контекстном меню появилась опция \"Open script\"
+                   которая позволяет быстро открыть js файл связанный с текущей открыторй странице. Для работы необходимо чтобы окно devtool
+                   было уже открыто`,
             "ua": "BPM. Відкриття сторінки в devtool",
             "en": "BPM. Opening a page in devtool"
         }
