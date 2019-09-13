0b8737fdce39a0eec7d31b342952a80c4152d3ca
Yurii Babenko <babenkomahalych@gmail.com>
2019-09-13 14:06:49 +0300

    feature/minor-fixes
    отформатировал

--- features/bpm-entity-info/bpm-entity-info.js
+++ features/bpm-entity-info/bpm-entity-info.js
@@ -1,17 +1,39 @@
 window.NSEntityInfo = {
-	container: null,
-	entity: null,
-	columnsProp: [
-		{name: "name", caption: "Назва", getter(item) {return item.name}},
-		{name: "caption", caption: "Caption", getter(item) {return item.caption}},
-		{name: "dataValueType", caption: "DataValueType", getter(item) {
-			return this.getPropNameByValue(Terrasoft.DataValueType, item.dataValueType);
-		}},
-		{name: "lookup", caption: "LookupSchemaName", getter(item) {return item.referenceSchemaName || "";}}
+	"container": null,
+	"entity": null,
+	"columnsProp": [
+		{
+			"name": "name",
+			"caption": "Назва",
+			getter(item) {
+				return item.name;
+			}
+		},
+		{
+			"name": "caption",
+			"caption": "Caption",
+			getter(item) {
+				return item.caption;
+			}
+		},
+		{
+			"name": "dataValueType",
+			"caption": "DataValueType",
+			getter(item) {
+				return this.getPropNameByValue(Terrasoft.DataValueType, item.dataValueType);
+			}
+		},
+		{
+			"name": "lookup",
+			"caption": "LookupSchemaName",
+			getter(item) {
+				return item.referenceSchemaName || "";
+			}
+		}
 	],
 
 	createElement(tagName, prop = {}) {
-		let el = document.createElement(tagName);
+		const el = document.createElement(tagName);
 		for (const key in prop) {
 			if (prop.hasOwnProperty(key)) {
 				el[key] = prop[key];
@@ -44,9 +66,9 @@ window.NSEntityInfo = {
 	},
 
 	handleDocumentClick() {
-		let scope = this;
-		let onClick = function() {
-			let clickedInsidePage = Boolean(event.target.closest("#ns-entity-info-cnt"));
+		const scope = this;
+		const onClick = function() {
+			const clickedInsidePage = Boolean(event.target.closest("#ns-entity-info-cnt"));
 			if (clickedInsidePage) {
 				return;
 			}
@@ -57,7 +79,7 @@ window.NSEntityInfo = {
 	},
 
 	addOpenListener() {
-		let scope = this;
+		const scope = this;
 		document.addEventListener("keyup", function(event) {
 			if (event.keyCode === 69 && event.shiftKey === true && event.ctrlKey === true) {
 				event.stopPropagation();
@@ -66,10 +88,10 @@ window.NSEntityInfo = {
 		});
 	},
 
-	loadEntityInfo({entityName}) {
+	loadEntityInfo({ entityName }) {
 		this.showInfoNotFound();
 		this.entity = null;
-		let scope = this;
+		const scope = this;
 		require([entityName], function(entity) {
 			scope.entity = entity;
 			scope.onEntityInfoLoaded(entity);
@@ -81,7 +103,7 @@ window.NSEntityInfo = {
 	},
 
 	generateColumnInfoTable(entity) {
-		let cnt = this.createElement("table", {className: "ns-ei-column-table"});
+		const cnt = this.createElement("table", { "className": "ns-ei-column-table" });
 		cnt.appendChild(this.generateColumnInfoHeader());
 		cnt.appendChild(this.generateColumnInfoContent(entity));
 		this.tableContainer.innerHTML = "";
@@ -89,19 +111,19 @@ window.NSEntityInfo = {
 	},
 
 	generateColumnInfoContent(entity) {
-		let content = this.createElement("tbody", {className: "ns-ei-column-table-content"});
-		let columns = Object.values(entity.columns);
-		let appenColumnInfoToContent = column => content.appendChild(this.createColumnInfoRow(column));
+		const content = this.createElement("tbody", { "className": "ns-ei-column-table-content" });
+		const columns = Object.values(entity.columns);
+		const appenColumnInfoToContent = column => content.appendChild(this.createColumnInfoRow(column));
 		columns.forEach(appenColumnInfoToContent, this);
 		this.tbody = content;
 		return content;
 	},
 
 	createColumnInfoRow(column) {
-		let row = this.createElement("tr", {className: "ns-ei-column-info-row"});
+		const row = this.createElement("tr", { "className": "ns-ei-column-info-row" });
 		row.setAttribute("column-name", column.name);
 		this.columnsProp.forEach(columnProp => {
-			let el = this.createElement("td");
+			const el = this.createElement("td");
 			el.innerText = columnProp.getter.call(this, column);
 			row.appendChild(el);
 		}, this);
@@ -109,11 +131,11 @@ window.NSEntityInfo = {
 	},
 
 	generateColumnInfoHeader() {
-		let header = this.createElement("thead", {className: "ns-ei-column-table-header"});
-		let captionRow = this.createElement("tr");
+		const header = this.createElement("thead", { "className": "ns-ei-column-table-header" });
+		const captionRow = this.createElement("tr");
 		header.appendChild(captionRow);
 		this.columnsProp.forEach(column => {
-			let el = this.createElement("th");
+			const el = this.createElement("th");
 			el.innerText = column.caption;
 			el.setAttribute("column-name", column.name);
 			captionRow.appendChild(el);
@@ -128,7 +150,7 @@ window.NSEntityInfo = {
 		this.container = cnt;
 		this.container.style.display = "none";
 		this.generateHeader();
-		this.tableContainer = this.createElement("div", {className: "ns-ei-table-cnt"});
+		this.tableContainer = this.createElement("div", { "className": "ns-ei-table-cnt" });
 		this.showInfoNotFound();
 		cnt.appendChild(this.tableContainer);
 		document.body.appendChild(cnt);
@@ -141,28 +163,28 @@ window.NSEntityInfo = {
 	},
 
 	getInfoNotFound() {
-		let cnt = this.createElement("span", {className: "ns-ei-info-not-found"});
+		const cnt = this.createElement("span", { "className": "ns-ei-info-not-found" });
 		cnt.innerText = "ENTITY NOT FOUND";
 		return cnt;
 	},
 
 	onEntityChanged(value) {
-		this.loadEntityInfo({entityName: value});
+		this.loadEntityInfo({ "entityName": value });
 	},
 
 	generateHeader() {
-		let header = this.createElement("div", {className: "ns-ei-header"});
+		const header = this.createElement("div", { 'className': "ns-ei-header" });
 		this.entityFilter = this.createHeaderFilter(header, "EntityName", this.onEntityChanged, this);
 		this.columnFilter = this.createHeaderFilter(header, "ColumnName", this.onColumnFilterChanged, this);
 		this.container.appendChild(header);
 	},
 
 	createHeaderFilter(header, caption, onEnter, scope) {
-		let cnt = this.createElement("span");
-		let text = this.createElement("span", {className: "ns-ei-filter-caption"});
+		const cnt = this.createElement("span");
+		const text = this.createElement("span", { "className": "ns-ei-filter-caption" });
 		text.innerText = caption;
 		cnt.appendChild(text);
-		let input = this.createElement("input");
+		const input = this.createElement("input");
 		input.addEventListener("keyup", (event) => {
 			if (event.keyCode === 13) {
 				event.preventDefault();
@@ -179,19 +201,18 @@ window.NSEntityInfo = {
 	},
 
 	filterColumns() {
-		let value = this.columnFilter.value.toLowerCase();
-		let filter = col => col.name.toLowerCase().includes(value) || col.caption.toLowerCase().includes(value);
+		const value = this.columnFilter.value.toLowerCase();
+		const filter = col => col.name.toLowerCase().includes(value) || col.caption.toLowerCase().includes(value);
 		if (!value) {
 			filter = () => true;
 		}
-		let columns = Object.values(this.entity.columns);
-		let visibleColumns = columns.filter(filter).map(col => col.name);
+		const columns = Object.values(this.entity.columns);
+		const visibleColumns = columns.filter(filter).map(col => col.name);
 		this.tbody.childNodes.forEach(item => {
-			let itemColumn = item.getAttribute("column-name");
-			let visible = visibleColumns.includes(itemColumn);
+			const itemColumn = item.getAttribute("column-name");
+			const visible = visibleColumns.includes(itemColumn);
 			item.style.display = visible ? "" : "none";
 		}, this)
 	}
 }
 NSEntityInfo.init();
-// NSEntityInfo.loadEntityInfo({entityName: "Contact"});
